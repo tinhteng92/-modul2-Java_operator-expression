e89f9974c141e64b7b26dae405f6e95675ac37a8
Chu Tinh <tinhteng@gmail>
2022-05-09 13:40:50 +0700

    java

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,24 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in); // khai báo đối tượng Scanner
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat(); // Nhập chiều rộng
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat(); // Nhập chiều cao
+
+        float area = width * height;
+
+        System.out.println("Area is: " + area);
+
+    }
+}
